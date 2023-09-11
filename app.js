961d7b15bcbe387b419be1a7c17f8f2a2a230a42
Hendrik Unger <unger.hendrik@gmail.com>
2023-09-11 21:23:09 +0200

    Created fist steps for datacallback

--- app.js
+++ app.js
@@ -49,11 +49,19 @@ var uint32 = ref.types.uint32
 var uint32Ptr = ref.refType(uint32)
 var int32 = ref.types.int32
 
-
+                                           
 const N1700InitializeLibrary = n1700.func('int __stdcall N1700InitializeLibrary(bool Console, _Out_ uint32 *NumModules, _Out_ uint32 *NumChannels, int32 Par)');
 const N1700FreeLibrary  = n1700.func('int __stdcall N1700FreeLibrary()');
 const N1700GetNumModules  = n1700.func('int __stdcall N1700GetNumModules()');
 const N1700GetNumChannels  = n1700.func('int __stdcall N1700GetNumChannels()');
+const N1700StartContinuousRequestAllData  = n1700.func('int __stdcall N1700StartContinuousRequestAllData(uint32 interval, int par)');
+const N1700StopContinuousRequestAllData  = n1700.func('int __stdcall N1700StopContinuousRequestAllData()');
+const N1700DataCallback = koffi.proto('int __stdcall N1700DataCallback(int numChannels, int *pChannelIdxArray, double* pData,void* pContext)');
+const N1700RegisterDataCallback = n1700.func('int __stdcall N1700RegisterDataCallback(N1700DataCallback *pCallback, int numChannels, int *pChannelIdxArray, void *pContext)');
+const N1700UnregisterDataCallback = n1700.func('int __stdcall N1700UnregisterDataCallback(N1700DataCallback *pCallback)');
+
+// TODO: Write the callback function in JS
+let cb_koffi_DataHandle = koffi.register(console.log, 'N1700DataCallback *');
 
 
 let modules = [5];
@@ -70,13 +78,40 @@ var ret = -5;
 ret = N1700GetNumModules();
 console.log("N1700GetNumModules ", ret);
 
-var ret = -5;
-ret = N1700GetNumChannels();
-console.log("N1700GetNumChannels ", ret);
+var nChannels = -5;
+nChannels = N1700GetNumChannels();
+console.log("N1700GetNumChannels ", nChannels);
 
 var ret = -5;
-ret = N1700FreeLibrary();
-console.log("N1700FreeLibrary ", ret);
+ret = N1700RegisterDataCallback(cb_koffi_DataHandle, nChannels, [0,1,2,3], 0);
+console.log("N1700RegisterDataCallback ", ret);
+
+var ret = -5;
+ret = N1700StartContinuousRequestAllData(1, 1);
+console.log("N1700StartContinuousRequestAllData ", ret);
+
+setTimeout(CleanUp, 5000);
+
+
+
+
+function CleanUp(){
+
+    var ret = -5;
+    ret = N1700StopContinuousRequestAllData();
+    console.log("N1700StopContinuousRequestAllData ", ret);
+
+    N1700UnregisterDataCallback(cb_koffi_DataHandle)
+    
+    koffi.unregister(cb_koffi_DataHandle);
+    
+    var ret = -5;
+    ret = N1700FreeLibrary();
+    console.log("N1700FreeLibrary ", ret);
+
+}
+
+
 
 
 
